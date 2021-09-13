45990859e4d22fda176ad7c5852124de883c44a8
Vinícius Valença <vvv@cesar.school>
2021-09-13 17:45:37 -0300

    atualizar método para pegar o token

--- rizumu.py
+++ rizumu.py
@@ -12,8 +12,4 @@ async def echo(ctx, *args):
     m_args = " ".join(args)
     await ctx.send(m_args)
 
-#token = ""
-#with open("token.txt") as file: 
-#    token = file.read()
-
 rizumu.run(os.getenv("TOKEN"))
